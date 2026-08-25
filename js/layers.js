ad37455f869f3434a38a3b43f902d03e1f4149df
omegaTreeMaker <omegatreemaker@gmail.com>
2026-08-25 12:36:09 +0200

    5

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("d", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.15, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -20,9 +20,14 @@ addLayer("d", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 0 // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "d, description: "D: Reset for descension points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+
+        },
+    },
 })
--- js/mod.js
+++ js/mod.js
@@ -21,7 +21,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added points.<br>
 		- Added descension.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, for now, or forever`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
