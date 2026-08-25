e87d4683e63ea9dfc2addd98e806baa584b46820
omegaTreeMaker <omegatreemaker@gmail.com>
2026-08-25 13:06:34 +0200

    8

--- js/layers.js
+++ js/layers.js
@@ -23,8 +23,8 @@ addLayer("d", {
         upgrades: {
         11: {
                 title: "doubler1",
-    description: "x2 point gain.",
-    cost: new Decimal(1),
+                description: "x2 point gain.",
+                cost: new Decimal(1),
         },
     },
     row: 0, 
