2bdf70a80ecebe8a5afe2bf82f3647031530d546
omegaTreeMaker <omegatreemaker@gmail.com>
2026-08-25 12:38:11 +0200

    8

--- js/layers.js
+++ js/layers.js
@@ -23,11 +23,11 @@ addLayer("d", {
     row: 0 // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "d, description: "D: Reset for descension points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ]
-    //layerShown(){return true},
-    //upgrades: {
-    //    11: {
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
 
-//        },
-  //  },
+        },
+    }
 })
